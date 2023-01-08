a0e8603c756be2014769178670e00b620b464e5b
merlienne <merliennep@hotmail.com>
2023-01-08 19:31:37 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+function toggleMode() {
+  const html = document.documentElement
+  // Uso da função toggle, resume a condição para o efeito de transição do modo dark para o light
+
+  html.classList.toggle("light")
+
+  /* Condição para o efeito de transição do modo dark para o light
+  if (html.classList.contains("light")) {
+    html.classList.remove("light")
+  } else {
+    html.classList.add("light")
+  }
+  */
+
+  // pegar a tag img
+  const img = document.querySelector("#profile img")
+
+  // substituir a imagem
+  if (html.classList.contains("light")) {
+    // se tiver em light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/Avatar-light.png")
+  } else {
+    // se tiver sem light mode, manter a imagem normal
+    img.setAttribute("src", "./assets/Avatar.png")
+  }
+}
